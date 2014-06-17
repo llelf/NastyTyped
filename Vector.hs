f632c6a84eb04a7bcc9d01040857a6f499685bfb
Antonio Nikishaev <a@lelf.me>
2014-06-17 04:56:36 +0400

    Eq Vector

--- Vector.hs
+++ Vector.hs
@@ -4,9 +4,25 @@
 import GHC.TypeLits
 import Data.Proxy
 
+
 data Vector (n::Nat) a where
     Nil :: Vector 0 a
     Cons :: a -> Vector n a -> Vector (n+1) a
+
+
+instance Eq a => Eq (Vector n a) where
+    Nil == Nil    = True
+    Nil == Cons{} = False
+    Cons{} == Nil = False
+    a == b        = eq a b
+        where
+          eq :: Eq a => Vector n1 a -> Vector n2 a -> Bool
+          eq Nil Nil    = True
+          eq Cons{} Nil = False
+          eq Nil Cons{} = False
+          eq (Cons a as) (Cons b bs) = a==b && eq as bs
+
+
 
 instance Show a => Show (Vector n a) where
     show (Cons x v) = "["++show x++" "++show v++"]"
@@ -19,6 +35,8 @@ v = Nil
 v1 :: Vector 1 Int
 v1 = Cons 42 Nil
 
+v2 :: Vector 2 Int
+v2 = Cons 42 (Cons 123 Nil)
 
 
 data Vector1 (n::Nat) a where
